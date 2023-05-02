1561fc5a77c176bd377ee8cea166ba2ca4903670
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-02 13:06:01 -0600

    build the to-do list app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
-import {v4 as uuidV4} from 'uuid'
+import { v4 as uuidV4 } from 'uuid';
 
-console.log(uuidV4())
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>('#list'),
+  form = document.getElementById('new-task-form') as HTMLFormElement | null,
+  input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(addListItem);
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (!input || input?.value === '' || input?.value === null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+
+  addListItem(newTask);
+
+  input.value = '';
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkBox = document.createElement('input');
+  checkBox.addEventListener('change', () => {
+    task.completed = checkBox.checked;
+    saveTasks();
+  });
+  checkBox.type = 'checkbox';
+  checkBox.checked = task.completed;
+
+  label.append(checkBox, task.title);
+  item.append(label);
+  list?.append(item);
+
+  saveTasks();
+}
+
+function saveTasks(): void {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const tasksJSON = localStorage.getItem('tasks');
+  if (tasksJSON === null) return [];
+
+  return JSON.parse(tasksJSON);
+}
